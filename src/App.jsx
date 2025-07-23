f3b5efc30b05dc7e2fe8dc6d734873178764e48f
NitinRana08 <nitinr8229@gmail.com>
2025-07-23 18:47:15 +0530

    .
    .

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ function App() {
     <div>
       <h1>hello, {userName}</h1>
       <p>{a+v}</p>
-      <h2>{313+313+3131+13}</h2>
+      {/* <h2>{313+313+3131+13}</h2> */}
+
+      <button onClick={()=>alert("hello")}>click</button>
 
     </div>
   )
