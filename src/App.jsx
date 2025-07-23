d36af70b119ec9f263e1e446d99281311a05635f
NitinRana08 <nitinr8229@gmail.com>
2025-07-23 18:45:04 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  const userName = 'nitin rana'
+  let a = 2
+  let v = 1
+  return (
+    <div>
+      <h1>hello, {userName}</h1>
+      <p>{a+v}</p>
+      <h2>{313+313+3131+13}</h2>
+
+    </div>
+  )
+}
+
+export default App
