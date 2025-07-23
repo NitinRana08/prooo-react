d1e1923a36171678251ca24fddef393ba9176937
NitinRana08 <nitinr8229@gmail.com>
2025-07-23 21:16:59 +0530

    .
    .

---
+++ src/ ToDoCom.jsx
@@ -0,0 +1,21 @@
+function ToDo() {
+
+    function callfun(){
+        alert("function called")
+    }
+    return (
+        <div>
+            <h1>nitin rnana</h1>
+            <img src="" alt="img" />
+
+            <ul>
+                <li>invent new traffic lighjt</li>
+                <li>Rehearse a movie technology</li>
+                <li>improve the spectrum technology</li>
+            </ul>
+            <button onClick={callfun}>Click Me</button>
+        </div>
+    )
+}
+
+export default ToDo;
--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
-import bullet from './assets/bullet.png'
+import ToDo from "./ ToDoCom";
+
 function App() {
-  
+
   return (
     <div>
-      <h1>nitin rana</h1>
-      <div>
-        <img src={bullet} alt="nitin rana" className="bullet-img" />
-      </div>
-
-      <ul>
-        <li>
-          Invent new trffic light
-        </li>
-        <li>Rehearse a movie scene</li>
-        <li>improve the spectrum technology</li>
-      </ul>
-      <button onClick={()=>alert("function called")}>click me</button>
-
+    <ToDo />
     </div>
-   
-   
+
   )
 }
 
