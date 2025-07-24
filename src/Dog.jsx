e9af41a2548a79c172f7b44a3a0803e4a78a6045
NitinRana08 <nitinr8229@gmail.com>
2025-07-25 04:30:57 +0530

    .
    .

--- src/Dog.jsx
+++ src/Dog.jsx
@@ -1,31 +1,57 @@
+//    let fruit = (name)=>{
+//        alert(name)
+
+//     }
 function Dog() {
-    const name = 'nitin ranaa'
-    let a = 2
-    let b = 6
-    const userr={
-        name:'nitin',
-        age:11,
-        mail: "nitinr8229@gmail.com"
 
-    }
-  
+    // function callFun(){
+    //     alert("called//")
+    // }
 
-    const hola = () => {
-        return 'apple'
-    }
-    let sum = (a, b) => {
-        return a + b
-    }
-    let cal = (a, b, calcu) => {
-        if (calcu == "+") {
-            return a + b
-        }
-        else if (calcu == '-') {
-            return a - b
-        }
-        else { }
+    // let fruit = (name)=>{
+    //    alert(name)
 
-    }
+    // }
+    // const name = 'nitin ranaa'
+    // let a = 2
+    // let b = 6
+    // const userr = {
+    //     name: 'nitin',
+    //     age: 11,
+    //     mail: "nitinr8229@gmail.com"
+
+    // }
+
+
+    // const hola = () => {
+    //     return 'apple'
+    // }
+    // let sum = (a, b) => {
+    //     return a + b
+    // }
+    // let cal = (a, b, calcu) => {
+    //     if (calcu == "+") {
+    //         return a + b
+    //     }
+    //     else if (calcu == '-') {
+    //         return a - b
+    //     }
+    //     else { }
+
+    // }
+
+    // const arr = ['nitn', 'rana', 'hello']
+    // const path = "https://cdn.shopify.com/s/files/1/0086/0795/7054/files/Labrador.jpg?v=1645179151"
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -34,20 +60,32 @@ function Dog() {
     return (
         <>
             {
-                <div>
-                    {/* <h1>{JSON.stringify(userr)}</h1> */}
-                    <h1>{userr.mail}</h1>
-                    {/* <h1>{helllo}</h1> */}
-                    <h1>nitin</h1>
-                    <h1>{name ? name : "user not found"}</h1>
-                    <h1>{a + b}</h1>
-                    <h1>{hola()}</h1>
-                    <h1>{sum(2, 5)}</h1>
-                    <h1>{cal(3, 3, "+")}</h1>
+                // <div>
+                //     <h1>enent and function call</h1>
+                //     {/* <button onClick={callFun}>click me </button> */}
+
+                //     <button onClick={()=>fruit("apple")}>apple</button>
+                //     <button onClick={()=>fruit("nitin's banana")}>bananaaaaaa</button>
+                // </div>
 
 
 
-                </div>
+                // <div>
+                //     {/* <h1>{JSON.stringify(userr)}</h1> */}
+                //     <h1>{userr.mail}</h1>
+                //     {/* <h1>{helllo}</h1> */}
+                //     <h1>nitin</h1>
+                //     <h1>{name ? name : "user not found"}</h1>
+                //     <h1>{a + b}</h1>
+                //     <h1>{hola()}</h1>
+                //     <h1>{sum(2, 5)}</h1>
+                //     <h1>{cal(3, 3, "+")}</h1>
+                //     <h1>{arr[1]}</h1>
+                //    <img src={path} alt="" />
+
+
+
+                // </div>
         /* <div><h1>nitin rana</h1></div>
         <div><img src="" alt="img hai bas" /></div>
         <div><ul>
