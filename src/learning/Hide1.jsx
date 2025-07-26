6cd24d98ae1ce42609ec43de76802c1edf83b807
NitinRana08 <nitinr8229@gmail.com>
2025-07-27 05:25:55 +0530

    .
    .

--- src/learning/Hide1.jsx
+++ src/learning/Hide1.jsx
@@ -1,13 +1,29 @@
 import { useState } from "react"
 
+
 function Hide1(){
 
-    const [display,setDisplay]=useState(1)
+    const [car,newcar]=useState("i20")
+
+    // const CAR=()=>{
+    //     if(car=="i20"){
+    //         newcar("thar")
+    //     }
+    //     else{
+    //         newcar("i20")
+    //     }
+    // }
+   const CAR = () => {
+  newcar(car === "i20" ? "thar" : "i20");
+};
 
     return(
         <>
         <h1>hello</h1>
-        <button>click to </button>
+        <h1>{car}</h1>
+        <button onClick={()=>CAR(newcar)}>click to </button>
+
+
         </>
 
 
