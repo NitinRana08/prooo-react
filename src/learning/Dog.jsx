190381c11bfaa89675abf698d61f4b7fe1c5fd1b
NitinRana08 <nitinr8229@gmail.com>
2025-07-27 07:27:01 +0530

    .
    .

--- src/learning/Dog.jsx
+++ src/learning/Dog.jsx
@@ -15,11 +15,7 @@ function Dog() {
 
 
 
-
     }
-
-
-
 
 return (
     <>
--- src/learning/Hide1.jsx
+++ src/learning/Hide1.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react"
 
 
-function Hide1(){
+function Hide1() {
 
-    const [car,newcar]=useState("i20")
+    const [car, newcar] = useState("i20")
 
     // const CAR=()=>{
     //     if(car=="i20"){
@@ -13,15 +13,26 @@ function Hide1(){
     //         newcar("i20")
     //     }
     // }
-   const CAR = () => {
-  newcar(car === "i20" ? "thar" : "i20");
-};
+    const CAR = () => {
+        newcar(car === "i20" ? "thar" : "i20");
+    };
+    
+    const [display, setDisplay] = useState()
 
-    return(
+
+    return (
         <>
-        <h1>hello</h1>
-        <h1>{car}</h1>
-        <button onClick={()=>CAR(newcar)}>click to </button>
+            <h1>hello</h1>
+            <button onClick={()=>setDisplay(!display)}>hidding</button>
+            {
+                display? <h1>hide wala</h1> :null
+            }
+
+
+
+            <h1>{car}</h1>
+            <button onClick={() => CAR(newcar)}>click to </button>
+            
 
 
         </>
