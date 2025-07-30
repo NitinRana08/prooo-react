9b31cc149807f8f8aab8db6689a622863fb4e79f
NitinRana08 <nitinr8229@gmail.com>
2025-07-30 11:48:30 +0530

    .
    .

--- src/learning/input_value/INPUTvalue.jsx
+++ src/learning/input_value/INPUTvalue.jsx
@@ -3,11 +3,11 @@ import { useState } from "react"
 function INPUTvalue(){
 
 
-    const [val,setVal]=useState("sharad rana")
+    const [val,setVal]=useState("")
     return(
         <>
         <h1>hello</h1>
-        <input type="text" onChange={(event)=>setVal(event.target.value)} placeholder="naam daal"/>
+        <input type="text" value={val} onChange={(event)=>setVal(event.target.value)} placeholder="naam daal"/>
         <h1>{val}</h1>
         <button onClick={()=>setVal("")}>click to remove item</button>
 
