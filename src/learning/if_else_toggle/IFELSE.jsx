c35cb2213401e31e7ccf2d0454124991e5cffc6f
NitinRana08 <nitinr8229@gmail.com>
2025-08-07 16:15:06 +0530

    .
    .

--- src/learning/if_else_toggle/IFELSE.jsx
+++ src/learning/if_else_toggle/IFELSE.jsx
@@ -1,28 +1,33 @@
 import { useState } from "react"
 
-function IFELSE() {
+function IFELSE(){
+    const [count,setCount]=useState(0)
 
 
-    const [count, setCount] = useState(0)
-    return (
-        
+    const a= "apple"
+    if(a=="apple"){
+         console.log("if wala")
+    }else{
+       console.log("else wala")
+    }
+    return(
         <>
-            <div>
-                <h1>hola</h1>
-                <h2>{count}</h2>
+        <div>
 
-                {
-                    count == 0 ? <h2>nitin rana</h2>:null
-                }
-                <button onClick={()=>setCount(count+1)}>counter</button>
-                <h2>@nitinrana</h2>
-
-                    </div>
-     
-        </>
-            )
+               
+               {/* <button onClick={()=>setCount(count+1)} >click to remove </button>
+            { 
+                count==0?<h1>yes count value is zero 0</h1>:
+                <h1>another number 1,2,3,3</h1>
            
+            }
+            <h3>{count}</h3> */}
+
+            {
+                a=="apple"? <h2>yes apple it is</h2>: <h2>na na</h2>
+            }
+        </div>
+        </>
+    )
 }
-
-
-            export default IFELSE
+export default IFELSE
