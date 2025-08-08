64bad901f3bf79ecd22eeeaf2db7a3f0009521ae
NitinRana08 <nitinr8229@gmail.com>
2025-08-08 17:13:10 +0530

    .
    .

--- src/learning/map/MAP.jsx
+++ src/learning/map/MAP.jsx
@@ -15,14 +15,36 @@ function MAP() {
 
         <div>
             <h1>aap sab k naam</h1>
-            {
+            <table border={1}>
+                <tr>
+                    <td>Name</td>
+                    <td>Age</td>
+                    <td>Location</td>
+                </tr>
+                {
+                    Student.map((data)=>
+                        <tr>
+                            <td>{data.name}</td>
+                            <td>{data.age}</td>
+                            <td>{data.location}</td>
+                        </tr>
+                    
+                    )
+                }
+
+            </table>
+
+
+
+
+
+
+            {/* {
             Student.map((data)=>
                 <h2>Name :  {data.name} <br /> Age : {data.age} <br />Location : {data.location}</h2> 
-               
-
-
+            
             )
-            }
+            } */}
         </div>
 
 
