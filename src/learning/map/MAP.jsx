a3e8cbbe41dd0a67ab7290fc89d7c4d4953cf8d1
NitinRana08 <nitinr8229@gmail.com>
2025-08-08 11:27:01 +0530

    .
    .

--- src/learning/map/MAP.jsx
+++ src/learning/map/MAP.jsx
@@ -4,18 +4,28 @@ function MAP() {
 
 
     const NormalArray = ['nitin', 'nikhil', 'sharad', 'shiv']
-
-    NormalArray.map((comeHERE) => {
-        console.log(comeHERE)
-
-    },[])
-
-
-
-
+    const Student = [
+        { name: 'nitin', age: 22, location: 'GNoida' },
+        { name: 'nikhil', age: 24, location: 'canada' },
+        { name: 'sharad', age: 22, location: 'delhi' }
+    ]
 
     return (
         <>
+
+        <div>
+            <h1>aap sab k naam</h1>
+            {
+            Student.map((data)=>
+                <h2>Name :  {data.name} <br /> Age : {data.age} <br />Location : {data.location}</h2> 
+               
+
+
+            )
+            }
+        </div>
+
+
 
 
 
