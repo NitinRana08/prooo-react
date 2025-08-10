8d4c6f018c06861be7b538861611120f0440b116
NitinRana08 <nitinr8229@gmail.com>
2025-08-10 09:15:17 +0530

    .
    .

--- src/learning/Form/FORM.jsx
+++ src/learning/Form/FORM.jsx
@@ -1,12 +1,26 @@
-function FORM(){
+function FORM (){
+
+    const handleIT=(e)=>{
+        console.log(e.target.value,e.target.name);
+        
+
+
+    }
     return(
         <>
-        <div>
-            <form action="">
-                <h2>fill the details</h2>
-            </form>
-        </div>
+        <h2>login form handling using useState</h2>
+        <form action="">
+            <div>
+                <input onChange={handleIT} type="emial" placeholder="email" aria-required/>
+                <label htmlFor="">Email</label>
+            </div>
+            <div>
+                <input type="password" placeholder="PS" required onChange={handleIT}/>
+                <label htmlFor="">password</label>
+            </div>
+            <button>submit</button>
 
+        </form>
         </>
     )
 }
