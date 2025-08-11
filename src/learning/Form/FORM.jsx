a8678d198bbb1250d248c903baa8ef2e592a9883
NitinRana08 <nitinr8229@gmail.com>
2025-08-11 09:05:59 +0530

    .

--- src/learning/Form/FORM.jsx
+++ src/learning/Form/FORM.jsx
@@ -1,25 +1,41 @@
+import { useState } from "react"
+
 function FORM() {
+     const [Fname,setFname]=useState("")
+        const [Lname,setLname]=useState("")
+        const [password,setPassword]=useState("")
 
 
-    const handlekroo= async()=>{
-        await new Promise(res=>setTimeout(res,2000))
-        console.log("submit ho gaya");
         
 
 
-    }
+
+        const handleInputChange=(comeData)=>{
+            setFname(comeData.target.value)
+            setLname(comeData.target.value)
+            setPassword(comeData.target.value)
+
+
+        }
+    
+
+
 
     return (
     <>
 
         <div>
             <h1>fill</h1>
-            <form action={handlekroo}>
-                <label htmlFor="">name: </label><input type="text" />
+            <form >
+                <h1>
+                <label htmlFor="">first name: </label><input type="text" value={Fname} onChange={handleInputChange} />
                 <br />
-                <label htmlFor="">password: </label><input type="text" />
+                <label htmlFor="">last name: </label><input type="text" value={Lname} onChange={handleInputChange}/>
+                <br />
+                <label htmlFor="">password: </label><input type="text" value={password} onChange={handleInputChange}/>
                 <br />
                 <button>submit</button>
+                </h1>
             </form>
         </div>
 
