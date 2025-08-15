26ffcadc4fef9729928460edb12f7c990a02003e
NitinRana08 <nitinr8229@gmail.com>
2025-08-15 20:59:38 +0530

    .
    .

--- src/learning/useRef/UseRefOne.jsx
+++ src/learning/useRef/UseRefOne.jsx
@@ -1,14 +1,14 @@
 import { useEffect, useState, useRef } from "react"
 
 function UseRefOne() {
-
+    w
 
 
 
     const [count, setCount] = useState(0)
     let a = useRef(0)
     useEffect(() => {
-         a.current=a.current+1
+        a.current = a.current + 1
         console.log(`the value is ${a.current}...`)
     })
 
@@ -19,7 +19,9 @@ function UseRefOne() {
         <>
             <h1>{count}</h1>
             <button onClick={() => setCount(count + 1)}>click me to update </button>
-
+            <h2>hi
+                
+            </h2>
         </>
     )
 }
