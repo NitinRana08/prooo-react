5ad90f6aa4a3e9575b42e1044e8acc7bbd913f51
NitinRana08 <nitinr8229@gmail.com>
2025-08-20 10:18:38 +0530

    .
    .

--- src/learning/useState/NEw.jsx
+++ src/learning/useState/NEw.jsx
@@ -1,7 +1,35 @@
-function NEw(){
-    return(
+import { useState, useEffect } from "react"
+function NEw() {
+    const [count, setCount] = useState(0)
+    const [first, setFirst] = useState(0)
+
+    useEffect(() => {
+        alert("hola ji")
+
+    }, [])
+
+    useEffect(() => {
+        alert("count btn ko chedaaaaa!!!!!")
+
+    }, [count])
+
+    useEffect(() => {
+        alert("first ko chedaaaaa!!!!!")
+
+    }, [first])
+
+
+
+
+    return (
         <>
-        <div>hola</div>
+            <div>
+
+                <div>hola</div>
+                <h1>here is the :{count}</h1>
+
+                <button onClick={() => setCount(count + 1)}>count btn </button>
+            </div>
         </>
     )
 }
