6e2fc78548082732478380be3daae29723a2f291
NitinRana08 <nitinr8229@gmail.com>
2025-09-04 04:05:31 +0530

    .
    .

--- src/learning/api/Appapi.jsx
+++ src/learning/api/Appapi.jsx
@@ -1,4 +1,14 @@
+import { useEffect } from 'react'
+import {getPosts} from './index'
+
 function Appapi(){
+
+    const [data,setData] = useState(null)
+
+    useEffect(()=>{
+        getPosts().then((posts)=>console.log(posts))
+    },[])
+
     return(
         <>
         <h2>hola ji</h2>
